70c6b6bf9b34d97daa98c5754c95730fa641b1ac
wowazun <ouskvulpes611@gmail.com>
2026-04-13 00:29:06 +0900

    一旦

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wowazun.net",
+  site: "https://wowazun.net",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
